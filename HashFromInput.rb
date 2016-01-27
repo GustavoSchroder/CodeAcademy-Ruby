ced9ba9f49d03a17742add58aa6dea05be35e5b6
Gustavo Lazarotto Schroeder <guto_schroeder@hotmail.com>
2016-01-27 13:56:32 -0200

    Create HashFromInput.rb

---
+++ HashFromInput.rb
@@ -0,0 +1,14 @@
+puts "Digite sua frase aqui"
+text = gets.chomp
+
+words = text.split(" ")
+
+frequencies = Hash.new(0)
+
+words.each { |word| frequencies [word] += 1}
+frequencies = frequencies.sort_by { |word, count| count }
+
+frequencies.reverse!
+
+frequencies.each { |word, frequency| puts word + ": " + frequency.to_s //to String == to_s
+}
